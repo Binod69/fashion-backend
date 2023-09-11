6054dd0814b8d73bcaeb9454942ff0c4c789f54f
Binod Budhathoki <100508632+Binod69@users.noreply.github.com>
2023-09-12 00:08:01 +0545

    added controllers for order

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,11 @@ const router = require('express').Router();
 const homeRoutes = require('./home.routes');
 const productRoutes = require('./products.routes');
 const userRoutes = require('./user.routes');
+const orderRoutes = require('./order.routes');
 
 router.use('/', homeRoutes);
 router.use('/products', productRoutes);
 router.use('/users', userRoutes);
+router.use('/orders', orderRoutes);
 
 module.exports = router;
