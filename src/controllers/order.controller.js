969e47b89906a16c0cee4c2fcee778951b21f575
Binod Budhathoki <100508632+Binod69@users.noreply.github.com>
2023-09-12 00:18:53 +0545

    added controlers

--- src/controllers/order.controller.js
+++ src/controllers/order.controller.js
@@ -5,21 +5,62 @@ const asyncHandler = require('express-async-handler');
 //@route POST api/v1/orders
 //@access Private
 const addOrderItems = asyncHandler(async (req, res) => {
-  res.send('add order items');
+  const {
+    orderItems,
+    shippingAddress,
+    paymentMethod,
+    itemsPrice,
+    taxPrice,
+    shippingPrice,
+    totalPrice,
+  } = req.body;
+
+  if (orderItems && orderItems.length === 0) {
+    res.status(400);
+    throw new Error('No order items');
+  } else {
+    const order = new Order({
+      orderItems: orderItems.map((x) => ({
+        ...x,
+        product: x._id,
+        _id: undefined,
+      })),
+      shippingAddress,
+      paymentMethod,
+      itemsPrice,
+      taxPrice,
+      shippingPrice,
+      totalPrice,
+    });
+
+    const createOrder = await order.save();
+
+    res.status(201).json(createOrder);
+  }
 });
 
 //@desc  Get logged in user orders
 //@route GET api/v1/orders/myorders
 //@access Private
 const getMyOrder = asyncHandler(async (req, res) => {
-  res.send('get my order ');
+  const orders = await Order.find({ user: req.user._id });
+  res.status(200).json(orders);
 });
 
 //@desc  Get order by id
 //@route GET api/v1/orders/:id
 //@access Private
 const getOrderById = asyncHandler(async (req, res) => {
-  res.send('get order by id');
+  const order = await Order.findById(req.params.id).populate(
+    'user',
+    'name,email'
+  );
+  if(order) {
+    res.status(200).json(order)
+  } else[
+    res.status(404);
+    throw new Error('Order not found')
+  ]
 });
 
 //@desc Update orders to paid
